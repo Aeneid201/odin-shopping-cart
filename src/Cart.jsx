229ddea8b5dfb836efa925e9ade3f0f40b944215
Aeneid201 <aloubi.oumaima@gmail.com>
2024-06-21 17:34:44 -0400

    cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,20 +1,33 @@
 import { useOutletContext } from "react-router-dom"
 import CartProduct from './components/CartProduct'
+import { useFetchSingle } from "./hooks/useFetch"
 
 export default function Cart() {
     const [cart, setCart] = useOutletContext()
+    const arr = [];
 
-    console.log(cart)
+    cart.map(item => {
+        const {product} = useFetchSingle(item.id)
+        arr.push(product)
+    })
+
+    function removeFromCart(id) {
+        console.log('remove item', id);
+        const index = arr.find(item => item.id === id)
+        arr.splice(index, 1)
+        return setCart(prevCart => prevCart.filter(item => item.id !== id))
+    }
 
     return(
         <section className="cart">
             <div className="container div row">
                 <div className="col-12">
-                   {/* {cart &&
-                    cart.map(product => {
-                        return (<CartProduct key={product.id} id={product.id} total={product.price} quantity="1" title={product.title} category={product.category} image={product.image} rating={product.rating.rate} price={product.price} />)
+                   {arr &&
+                    arr.map(product => {
+                        //const itemFromCart = cart.find(item => item.id === product.id) ?? {}
+                        return (<CartProduct onClick={() => removeFromCart(product.id)} key={product.id} id={product.id} total={product.price} quantity="1" title={product.title} category={product.category} rating={product.rating?.rate} image={product.image}  price={product.price} />)
                     })
-                   } */}
+                   }
                 </div>
             </div>
         </section>
