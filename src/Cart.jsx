66884de0b5f49b01cb4f603dc969a5605dca644c
Aeneid201 <aloubi.oumaima@gmail.com>
2024-06-21 16:39:09 -0400

    cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,19 +4,17 @@ import CartProduct from './components/CartProduct'
 export default function Cart() {
     const [cart, setCart] = useOutletContext()
 
-    function removeFromCart(id) {
-        return setCart(prevCart => prevCart.filter(p => p.id !== id))
-    }
+    console.log(cart)
 
     return(
         <section className="cart">
             <div className="container div row">
                 <div className="col-12">
-                   {cart &&
+                   {/* {cart &&
                     cart.map(product => {
                         return (<CartProduct key={product.id} id={product.id} total={product.price} quantity="1" title={product.title} category={product.category} image={product.image} rating={product.rating.rate} price={product.price} />)
                     })
-                   }
+                   } */}
                 </div>
             </div>
         </section>
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -2,14 +2,23 @@ import Product from "./components/Product"
 import {useOutletContext } from "react-router-dom"
 import { useFetchAll } from "./hooks/useFetch"
 
-
 export default function Shop() {
 
 const {products, error, loading} = useFetchAll()
 const [cart, setCart] = useOutletContext()
 
 function addToCart(p) {
-  setCart(prevCart => [...prevCart, p])
+  setCart(prevCart => {
+    if(prevCart.some(item => item.id === p.id)){
+      return prevCart.map(item => {
+        const newObj = Object.assign({}, item) // to avoid duplication
+        if(item.id === p.id) return {...newObj, quantity: newObj.quantity+=1};
+        else return newObj
+      })
+    }else {
+      return [...prevCart, {id: p.id, quantity: 1}]
+    }
+  })
 }
 
   if(loading) return <p>Loading...</p>
